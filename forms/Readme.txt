38a2b2a7ed8a6b1c5774e2d874073dc88a1208a9
RAHAJASON Aroniaina Saotra <89457263+AroniainaSaotra@users.noreply.github.com>
2022-05-17 13:02:27 +0300

    change4

--- forms/Readme.txt
+++ forms/Readme.txt
@@ -1,2 +1,4 @@
 ETU001121
 User/Password : admin / admin
+
+lien du site: http://saotrabe15.alwaysdata.net/
--- forms/connection.php
+++ forms/connection.php
@@ -2,7 +2,8 @@
 function connect() {
     static $connect = null;
     if ($connect === null) {
-        $connect = mysqli_connect('localhost', 'root', 'root', 'rechauffement');
+        //$connect = mysqli_connect('localhost', 'root', 'root', 'rechauffement');
+        $connect = mysqli_connect('mysql-saotrabe15.alwaysdata.net', '268543_user', 'Zj34f3qCW!qhd4R', 'saotrabe15_rc');
         $connect->set_charset("utf8");
     }
     return $connect;
